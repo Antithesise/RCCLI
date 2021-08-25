e9e57a9fa06aab6a5f17141701ec97a49edc9a0e
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2021-08-26 08:06:59 +1000

    added realtime syntaxn highlighting

--- CommandLine.py
+++ CommandLine.py
@@ -19,6 +19,12 @@ class CommandLine:
         
         self.commands = {}
 
+    def reset(self):
+        from termios import tcsetattr, TCSADRAIN
+        from sys import stdin
+
+        tcsetattr(stdin.fileno, TCSADRAIN, self.__old_settings)
+
     def command(self, *args, **kwargs):
         def decorator_command(func):
             from functools import wraps
@@ -54,34 +60,95 @@ class CommandLine:
                 return settings["onerror"]()
 
     def __call__(self):
+        from termios import tcgetattr
+        from sys import stdin, stdout
+        from Getpass import getpass
+        from Syntax import Syntax
+        from tty import setraw
+        from os import getenv
+
         settings = self.config
 
         while True:
-            try:
-                uin = input(settings["prompt"])
-                ans = [s.strip() for s in uin.split(" ") if s.strip()]
-            except EOFError as E:
-                if settings["eofexit"]:
-                    return settings["atexit"]()
-                else:
-                    raise EOFError()
-            except KeyboardInterrupt as E:
-                if settings["interruptexit"]:
-                    return settings["atexit"]()
-                else:
-                    raise KeyboardInterrupt()
-            except Exception:
-                return settings["onerror"]()
+            self.auth = getpass("Password: ")
+            if self.auth == getenv("CODE"):
+                break
+            print("Incorrect!")
 
-            if not ans:
+        self.__syntax = self.Syntax(self) # create instance of Syntax class
+        self.__old_settings = tcgetattr(stdin) # store old tty settings
+
+        while True:
+            uinput = ""
+            self.__index = 0
+
+            print(end=settings["prompt"])
+
+            while True:
+                setraw(stdin) # stop tty from accessing stdin
+
+                char = ord(stdin.read(1))
+
+                if char == 3: # ctrl+c
+                    self.reset()
+                    print()
+
+                    if settings["interruptexit"]:
+                        return settings["atexit"]()
+                    else:
+                        break
+                if char == 5: # ctrl+d
+                    self.reset()
+                    print()
+
+                    if settings["eofexit"]:
+                        return settings["atexit"]()
+                    else:
+                        raise EOFError()
+                elif char in [10, 13]: # enter
+                    if self.__syntax(uinput, underline=True, valid=True):
+                        stdout.write("\r\nechoing... " + uinput + "\n\r")
+                        self.reset()
+                        print()
+
+                        break
+                    else:
+                        print(end="\a")
+                elif char == 27: # arrow keys
+                    next1, next2 = ord(stdin.read(1)), ord(stdin.read(1))
+
+                    if next1 == 91:
+                        if next2 == 68: # Left
+                            self.__index = max(0, self.__index - 1)
+                        elif next2 == 67: # Right
+                            self.__index = min(len(uinput), self.__index + 1)
+                elif char in range(32, 127):
+                    uinput = uinput[:self.__index] + chr(char) + uinput[self.__index:]
+
+                    self.__index += 1
+                elif char == 127: # delete
+                    uinput = uinput[:self.__index - 1] + uinput[self.__index:]
+
+                    self.__index -= 1
+
+                uinput = self.__syntax(uinput, False)
+
+                stdout.write("\r\u001b[0K" + self.__syntax(uinput) + "\r" + ("\u001b[" + str(self.__index) + "C" if self.__index > 0 else ""))
+                stdout.flush()
+
+                self.reset()
+
+            uin = [s.strip() for s in uinput.split(" ") if s.strip()]
+
+            if not uin:
                 continue
 
-            cmd = ans[0]
+            cmd = uin[0]
             args = []
             kwargs = {}
 
-            if len(ans) > 1:
-                args = ans[1:]
+            if len(uin) > 1:
+                args = uin[1:]
                 
                 for a in args:
                     if a.count(":") > 0:
