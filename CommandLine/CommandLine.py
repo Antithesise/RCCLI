feb8e8d149644873fad2959f718cfce9bde48796
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2021-08-27 20:28:32 +1000

    prevented some imports importing during runtime

--- CommandLine/CommandLine.py
+++ CommandLine/CommandLine.py
@@ -1,4 +1,8 @@
-from typing import Any
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING: # import for typechecking, but not at runtime
+    from typing import Any
 
 class CommandLine:
     """
