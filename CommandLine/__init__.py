73b037dfbe9b1ecd481161c9b9c7f1096fec0341
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2021-08-29 10:10:22 +1000

    moved ext import deletions to ext/__init__.py

--- CommandLine/__init__.py
+++ CommandLine/__init__.py
@@ -6,6 +6,5 @@ from CommandLine.CommandLine import *
 from CommandLine import ext
 
 del TYPE_CHECKING # remove TYPE_CHECKING constant
-del ext.TYPE_CHECKING
 
 del ext.check # remove function from CheckUnix
--- CommandLine/ext/__init__.py
+++ CommandLine/ext/__init__.py
@@ -1,3 +1,5 @@
 from CommandLine.ext.Getpass import *
 from CommandLine.ext.Syntax import *
 from CommandLine.ext.CheckUnix import check
+
+del TYPE_CHECKING # remove TYPE_CHECKING constant
